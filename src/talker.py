5e46c4307506aceb53e2ee94067aa7d9230a96c6
Happy-wada <c1100471@planet.kanazawa-it.ac.jp>
2021-12-07 17:53:10 +0900

    first commit

---
+++ src/talker.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+import rospy 
+import time
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+class EnterRoom():
+    def __init__(self):
+        self.twist_pub = rospy.Publisher('/vmegarover/diff_drive_controller/cmd_vel',Twist, queue_size = 1)
+
+        
+        rospy.Subscriber('/scan', LaserScan, self.laserCB)
+
+        self.front_laser_dist = 999.9
+        
+        
+    
+     
+    def laserCB(self, receive_msg):
+        self.front_laser_dist = receive_msg.ranges[359]
+        
+        
+    def execute(self):
+        
+        
+        vel = Twist()
+        vel.linear.x = 0.5
+        target_dist = 5
+        safe_dist = 3.0
+        target_time = target_dist / vel.linear.x
+        start_time = time.time()
+        stop_time = 0
+        stop_start = 0
+        stop_end = 0
+        
+
+        while not rospy.is_shutdown():
+            
+            
+            if self.front_laser_dist >= safe_dist   :
+                self.twist_pub.publish(vel)
+                print("Go straight")
+
+    
+            elif self.front_laser_dist <= safe_dist:
+                stop_start = time.time()
+                print"Disability in front of you "
+                rospy.sleep(1.0)
+                stop_end = time.time() - stop_start
+                stop_time += stop_end
+                
+            finish_time = time.time() - start_time - stop_time
+            if finish_time >= target_time:
+                print'Completed entry'
+                break
+
+        
+if __name__ == '__main__':
+    rospy.init_node('door_open')
+    
+    er = EnterRoom()
+    
+    er.execute()
+    rospy.spin()
+
+    
+
